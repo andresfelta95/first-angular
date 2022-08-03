65869dbaee1b64d82ea7e28d7a229ba668cc7261
andresfelta95 <andresfelta95@gmail.com>
2022-08-03 10:35:30 -0400

    Basic form complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 
+type Movie = {
+  name: string,
+  available: number,
+  quantity: number
+};
+type Movies = Array<Movie>;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-angular';
+  title: string = 'Movies';
+  movies: Movies = [
+    {
+      name: "Thor Love & Thunder",
+      available: 10,
+      quantity: 0
+    },
+    {
+      name: "Minions",
+      available: 50,
+      quantity: 0
+    }
+  ];
+  addMovieQuantity(movieName: string){
+    const movieIndex = this.movies.findIndex(movie => movie.name === movieName);
+    this.movies[movieIndex].quantity++;
+  }
+  removeMovieQuantity(movieName: string){
+    const movieIndex = this.movies.findIndex(movie => movie.name === movieName);
+    this.movies[movieIndex].quantity--;
+  }
 }
